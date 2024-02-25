5e183ce585922080a619da6735f243c832b39306
W2skaran <karanribadiya556@gmail.com>
2024-02-25 21:28:54 +0530

    first commit

---
+++ src/utils/useCard.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { FETCH_MENU_URL } from "../config";
+const useCard = (id) => {
+  const [card, setCard] = useState(null);
+
+  useEffect(() => {
+    getCardInfo();
+  }, []);
+
+  async function getCardInfo() {
+    try {
+      const headers = {'Content-Type':'application/json',
+                    'Access-Control-Allow-Origin':'*',
+                    'Access-Control-Allow-Methods':'GET,POST,PATCH,OPTIONS'}
+      const response = {
+          statusCode: 200,
+          headers:headers,
+          body: JSON.stringify(X),
+      };
+      const data = await fetch(FETCH_MENU_URL + id);
+      if (!data.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const json = await data.json();
+      // console.log( json?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[4]?.card
+      //   ?.card?.categories[0]?.itemCards)
+      setCard(
+        json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
+        ?.card?.itemCards
+      );
+    } catch (error) {
+      console.error("Error fetching restaurant data:", error.message);
+      // You can add additional error handling here
+    }
+    //setRestraunt(json?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[3]?.card?.card?.categories[1]?.itemCards[0]);
+  }
+  return card;
+};
+
+export default useCard;
---
+++ src/utils/useRestraunt.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { FETCH_MENU_URL } from "../config";
+const useRestraunt = (id) => {
+  const [restaurant, setRestraunt] = useState(null);
+
+  useEffect(() => {
+    getRestrauntInfo();
+  }, []);
+
+  async function getRestrauntInfo() {
+    const headers = {'Content-Type':'application/json',
+                    'Access-Control-Allow-Origin':'*',
+                    'Access-Control-Allow-Methods':'GET,POST,PATCH,OPTIONS'}
+      const response = {
+          statusCode: 200,
+          headers:headers,
+          body: JSON.stringify(X),
+      };
+    const data = await fetch(FETCH_MENU_URL + id);
+    const json = await data.json();
+    //console.log(json?.data);
+    setRestraunt(json?.data?.cards[2]?.card?.card?.info);
+  }
+  return restaurant;
+};
+
+export default useRestraunt;
